9bd69b27989cfc57d76594e2eeeb81006bbd2e35
prince-tabuzo <tabuzoprinceesoj@gmail.com>
2024-02-15 21:44:35 +0800

    v.0.4

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 
+//<Hero />
+
 function App() {
   return (
     <div className="app">
       <Nav />
-      <Hero />
     </div>
   );
 }
