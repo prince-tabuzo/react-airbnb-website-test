a1af435dbe85b7d262770da1197a00aec38f4969
prince-tabuzo <tabuzoprinceesoj@gmail.com>
2024-02-25 22:25:23 +0800

    v0.9

--- src/App.tsx
+++ src/App.tsx
@@ -2,66 +2,29 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import CardData from "./components/CardData";
 
 //
 
 function App() {
+  const cardElements = CardData.map((cardData) => {
+    return (
+      <Card
+        state={cardData.state}
+        img={cardData.img}
+        title={cardData.title}
+        rating={cardData.rating}
+        rating_num={cardData.rating_num}
+        price={cardData.price}
+      />
+    );
+  });
+
   return (
     <div className="app">
       <Nav />
       <Hero />
-      <div className="card-div">
-        <div>
-          <Card
-            state="SOLD OUT"
-            img="card_content_image_1.png"
-            title="Life lessons with Katie Zaferes"
-            rating={5.0}
-            rating_num={6}
-            price={136}
-          />
-        </div>
-        <div>
-          <Card
-            state="AVAILABLE"
-            img="card_content_image_2.png"
-            title="Learn wedding photography"
-            rating={5.0}
-            rating_num={30}
-            price={125}
-          />
-        </div>
-        <div>
-          <Card
-            state="SOLD OUT"
-            img="card_content_image_3.png"
-            title="Creative Cooking"
-            rating={4.7}
-            rating_num={26}
-            price={140}
-          />
-        </div>
-        <div>
-          <Card
-            state="AVAILABLE"
-            img="card_content_image_4.png"
-            title="Mastering Meditation"
-            rating={4.5}
-            rating_num={12}
-            price={175}
-          />
-        </div>
-        <div>
-          <Card
-            state="SOLD OUT"
-            img="card_content_image_5.png"
-            title="Gardening Guidance"
-            rating={4.3}
-            rating_num={23}
-            price={100}
-          />
-        </div>
-      </div>
+      <div className="card-div">{cardElements}</div>
       <br />
       <br />
       <br />
---
+++ src/components/CardData.tsx
@@ -0,0 +1,42 @@
+export default [
+  {
+    state: "SOLD OUT",
+    img: "card_content_image_1.png",
+    title: "Life lessons with Katie Zaferes",
+    rating: 5.0,
+    rating_num: 6,
+    price: 136,
+  },
+  {
+    state: "AVAILABLE",
+    img: "card_content_image_2.png",
+    title: "Learn wedding photography",
+    rating: 5.0,
+    rating_num: 30,
+    price: 125,
+  },
+  {
+    state: "SOLD OUT",
+    img: "card_content_image_3.png",
+    title: "Creative Cooking",
+    rating: 4.7,
+    rating_num: 26,
+    price: 140,
+  },
+  {
+    state: "AVAILABLE",
+    img: "card_content_image_4.png",
+    title: "Mastering Meditation",
+    rating: 4.5,
+    rating_num: 12,
+    price: 175,
+  },
+  {
+    state: "SOLD OUT",
+    img: "card_content_image_5.png",
+    title: "Gardening Guidance",
+    rating: 4.3,
+    rating_num: 23,
+    price: 100,
+  },
+];
